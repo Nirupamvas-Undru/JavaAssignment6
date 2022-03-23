00a64a1c2381bd889b5bd00d8cf580b5e3afdbaf
Nirupamvas-Undru <nirupamvas.undru@zemosolabs.com>
2022-03-23 23:39:01 +0530

    Assignment 6 files

---
+++ src/Question4.java
@@ -0,0 +1,15 @@
+class Question3 {
+
+    Question3(String str) {
+        System.out.println(str);
+    }
+
+};
+public class Question4 {
+
+    public static void main(String[] args) {
+        Question3[] obj=new Question3[2];
+        obj[0]=new Question3("Nirupamvas");
+        obj[1]=new Question3("Nirupmvas-Undru");
+    }
+}
